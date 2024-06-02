b2b2ea058bd6f454b25a118cd1aff6aaaa1fcd6c
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-02 18:43:37 +0300

    Add header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <b>turkishairlines.com</b>
+      </div>
+      <div>
+        <small>search</small>
+        <span>Flight Challange</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,11 @@
+import Header from "../../components/header/Header";
+
 const Home = () => {
-  return <div className="home">Hello world</div>;
+  return (
+    <div className="home">
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
