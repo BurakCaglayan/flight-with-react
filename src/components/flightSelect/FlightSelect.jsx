ada20fcd3bb8303b8f5e622b6d1b3f9bb11af15f
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-03 15:42:32 +0300

    Add DatePicker component to FlightSelect component

--- src/components/flightSelect/FlightSelect.jsx
+++ src/components/flightSelect/FlightSelect.jsx
@@ -1,4 +1,5 @@
 import Dropdown from "../UI/Dropdown";
+import DatePicker from "../UI/DatePicker";
 
 const FlightSelect = () => {
   const options = [
@@ -16,7 +17,9 @@ const FlightSelect = () => {
       <div className="flight-select__dropdown">
         <Dropdown labelText={"Nereye"} options={options} searchBy={"name"} />
       </div>
-      <div>Date area</div>
+      <div>
+        <DatePicker />
+      </div>
       <div>Count area</div>
       <div>Button</div>
     </div>
