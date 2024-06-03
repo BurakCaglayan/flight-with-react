ee12d7a42c97639e22a44c1963599e56718c13a4
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-03 22:11:50 +0300

    Update searchFilter for airports list

--- src/utils/common.js
+++ src/utils/common.js
@@ -3,7 +3,7 @@ import flightsData from "./flights.json";
 export const searchFilter = ({ searchValue, list, searchBy = "name" }) => {
   let lowerCaseQuery = searchValue.toLowerCase();
   let filteredList = searchValue
-    ? list.filter((x) => x[searchBy].name.toLowerCase().includes(lowerCaseQuery))
+    ? list.filter((x) => x[searchBy].toLowerCase().includes(lowerCaseQuery))
     : list;
   return filteredList;
 };
