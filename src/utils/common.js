33281a6d9417d12ad0cacc897a45d63108715197
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-03 21:38:07 +0300

    Update common.js files with data

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,7 +1,11 @@
+import flightsData from "./flights.json";
+
 export const searchFilter = ({ searchValue, list, searchBy = "name" }) => {
   let lowerCaseQuery = searchValue.toLowerCase();
   let filteredList = searchValue
-    ? list.filter((x) => x[searchBy].toLowerCase().includes(lowerCaseQuery))
+    ? list.filter((x) => x[searchBy].name.toLowerCase().includes(lowerCaseQuery))
     : list;
   return filteredList;
 };
+
+export const getFlights = () => flightsData.flights;
