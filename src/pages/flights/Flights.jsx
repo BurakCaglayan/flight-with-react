80feed29c2ad5c2afade264021eb6b5ff38ed58a
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-03 23:59:27 +0300

    Update general structor to Flights page

--- src/pages/flights/Flights.jsx
+++ src/pages/flights/Flights.jsx
@@ -5,6 +5,15 @@ const Flights = () => {
   return (
     <div className="flights">
       <Header className={"header__dark"} />
+      <div className="flights__container">
+        <small className="flights__title">Uçuş</small>
+        <div className="flights__info">İstanbul Antalya 6 yolcu</div>
+        <div className="flights__promo">
+          <small className="flights__promo__title">Promosyon kodu</small>
+          <div>Switch button</div>
+        </div>
+        <div>Uçuş seçim kartı</div>
+      </div>
     </div>
   );
 };
