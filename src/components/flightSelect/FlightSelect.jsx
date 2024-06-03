180f6669c5db0a2418f63e07f01b1c21c0fe0ad9
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-03 15:01:01 +0300

    Add Dropdown components & test data to FlightSelect component

--- src/components/flightSelect/FlightSelect.jsx
+++ src/components/flightSelect/FlightSelect.jsx
@@ -1,8 +1,21 @@
+import Dropdown from "../UI/Dropdown";
+
 const FlightSelect = () => {
+  const options = [
+    { id: 1, name: "İstanbul" },
+    { id: 2, name: "Ankara" },
+    { id: 3, name: "Antalya" },
+    { id: 4, name: "İzmir" },
+    { id: 5, name: "Bursa" },
+  ];
   return (
     <div className="flight-select">
-      <div>Dep area</div>
-      <div>Arr area</div>
+      <div>
+        <Dropdown labelText={"Nereden"} options={options} searchBy={"name"} />
+      </div>
+      <div>
+        <Dropdown labelText={"Nereye"} options={options} searchBy={"name"} />
+      </div>
       <div>Date area</div>
       <div>Count area</div>
       <div>Button</div>
