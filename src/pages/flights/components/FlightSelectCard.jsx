bd4df83c6fabab6b49c7e1dfa9de50565fba9ba7
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-04 07:24:14 +0300

    Refactor set final amount to local storage

--- src/pages/flights/components/FlightSelectCard.jsx
+++ src/pages/flights/components/FlightSelectCard.jsx
@@ -1,13 +1,22 @@
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
 const FlightSelectCard = ({ className, item, isToggle }) => {
+  const navigate = useNavigate();
+
   const { brandCode, price, rights } = item;
   const amount =
     isToggle && brandCode === "ecoFly" ? price.amount / 2 : price.amount;
 
   const handleFlightSelectClick = () => {
-    localStorage.setItem("amount", amount);
+    const finalPrice = {
+      amount,
+      currency: price.currency,
+    };
+    localStorage.setItem("finalPrice", JSON.stringify(finalPrice));
+    navigate("/result");
   };
+
   return (
     <div className={`flight-select-card ${className}`}>
       <div className="flight-select-card__header">
@@ -30,7 +39,7 @@ const FlightSelectCard = ({ className, item, isToggle }) => {
       <div className="flight-select-card__footer">
         <button
           className={"flight-select-card__button"}
-          onClick={() => handleFlightSelectClick}
+          onClick={handleFlightSelectClick}
           disabled={isToggle && brandCode !== "ecoFly"}
         >
           Uçuşu Seç
