0241bbc6bf083a107f4093ae471ec422c2149c99
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-04 07:45:10 +0300

    Update Result page with data

--- src/pages/result/Result.jsx
+++ src/pages/result/Result.jsx
@@ -1,10 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import Header from "../../components/header/Header";
-import FlightSelect from "../../components/flightSelect/FlightSelect";
 
 const Result = () => {
+  const finalPrice = JSON.parse(localStorage.getItem("finalPrice"));
+  const { count } = JSON.parse(localStorage.getItem("passengersSelections"));
+
   return (
     <div className="result">
       <Header className={"header__dark"} />
+      <div className="result__container">
+        <div className="result__body">
+          <FontAwesomeIcon
+            icon={faCircleCheck}
+            className="result__body__success-icon"
+          />
+          <div>Kabin seçiminiz tamamlandı</div>
+        </div>
+        <hr />
+        <div className="result__footer">
+          <div>Toplam Tutar</div>
+          <div className="result__footer__amount">
+            <div>{finalPrice.currency}</div>
+            <div>{finalPrice.amount * count}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
