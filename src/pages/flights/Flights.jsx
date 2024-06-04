14558ec862b3c73e25e86492ca71bf1fa99bb76f
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-04 03:44:29 +0300

    Update city and passengers count info with localstorage

--- src/pages/flights/Flights.jsx
+++ src/pages/flights/Flights.jsx
@@ -2,13 +2,22 @@ import FlightsList from "./components/FlightsList";
 import Header from "../../components/header/Header";
 
 const Flights = () => {
-  // console.log(JSON.parse(localStorage.getItem("selectedFlights")));
+  const { originAirport, destinationAirport } = JSON.parse(
+    localStorage.getItem("selectedFlights")
+  )[0];
+  const passengersSelections = JSON.parse(
+    localStorage.getItem("passengersSelections")
+  );
+  const originCityName = originAirport.city.name;
+  const destinationCityName = destinationAirport.city.name;
+  const { count } = passengersSelections;
   return (
     <div className="flights">
       <Header className={"header__dark"} />
       <div className="flights__container">
         <small className="flights__title">Uçuş</small>
-        <div className="flights__info">İstanbul Antalya 6 yolcu</div>
+        <div className="flights__info">{`${originCityName} - ${destinationCityName}, ${count} yolcu
+        `}</div>
         <div className="flights__promo">
           <small className="flights__promo__title">Promosyon kodu</small>
           <div>Switch button</div>
