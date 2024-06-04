572c00211bcad1150178c4fe7cb99f1e2bcdfa34
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-04 05:07:13 +0300

    UpdateFlightInfo component with data

--- src/pages/flights/components/FlightInfo.jsx
+++ src/pages/flights/components/FlightInfo.jsx
@@ -1,22 +1,29 @@
-const FlightInfo = () => {
+const FlightInfo = ({ flight }) => {
+  const {
+    arrivalDateTimeDisplay,
+    departureDateTimeDisplay,
+    flightDuration,
+    originAirport,
+    destinationAirport,
+  } = flight;
   return (
     <div className="flight-info">
       <div className="flight-info__dep">
-        <div>11.20</div>
-        <div>short</div>
-        <div>full</div>
+        <div>{departureDateTimeDisplay}</div>
+        <div>{originAirport.city.code}</div>
+        <div>{originAirport.city.name}</div>
       </div>
       <div className="flight-info__div">
         <hr class="solid" />
       </div>
       <div className="flight-info__arr">
-        <div>11.20</div>
-        <div>short</div>
-        <div>full</div>
+        <div>{arrivalDateTimeDisplay}</div>
+        <div>{destinationAirport.city.code}</div>
+        <div>{destinationAirport.city.name}</div>
       </div>
       <div className="flight-info__flight">
-        <div>süre</div>
-        <div>1h 30m</div>
+        <div>Uçuş Süresi</div>
+        <div>{flightDuration}</div>
       </div>
     </div>
   );
