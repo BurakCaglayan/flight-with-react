fa199e9dbb1fd50b1a43bed9efba8e4f5171695f
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-04 08:41:34 +0300

    Add error status to result page

--- src/pages/result/Result.jsx
+++ src/pages/result/Result.jsx
@@ -5,26 +5,45 @@ import Header from "../../components/header/Header";
 const Result = () => {
   const finalPrice = JSON.parse(localStorage.getItem("finalPrice"));
   const { count } = JSON.parse(localStorage.getItem("passengersSelections"));
+  const errorStatus = localStorage.getItem("errorStatus");
 
   return (
     <div className="result">
       <Header className={"header__dark"} />
       <div className="result__container">
-        <div className="result__body">
-          <FontAwesomeIcon
-            icon={faCircleCheck}
-            className="result__body__success-icon"
-          />
-          <div>Kabin seçiminiz tamamlandı</div>
-        </div>
-        <hr />
-        <div className="result__footer">
-          <div>Toplam Tutar</div>
-          <div className="result__footer__amount">
-            <div>{finalPrice.currency}</div>
-            <div>{finalPrice.amount * count}</div>
-          </div>
-        </div>
+        {errorStatus !== "error" ? (
+          <>
+            <div className="result__body">
+              <FontAwesomeIcon
+                icon={faCircleCheck}
+                className="result__body__success-icon"
+              />
+              <div>Kabin seçiminiz tamamlandı</div>
+            </div>
+            <hr />
+            <div className="result__footer">
+              <div>Toplam Tutar</div>
+              <div className="result__footer__amount">
+                <div>{finalPrice.currency}</div>
+                <div>{finalPrice.amount * count}</div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="result__body">
+              <FontAwesomeIcon
+                icon={faCircleCheck}
+                className="result__body__error-icon"
+              />
+              <div>Kabin seçiminiz tamamlanamadı.</div>
+            </div>
+            <hr />
+            <div className="result__footer-error">
+              <button className="result__footer-error__button">Başa Dön</button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
