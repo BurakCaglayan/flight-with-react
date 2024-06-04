b5e67b3039df31cca03bf8e11693bb8a36405cf5
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-04 07:07:12 +0300

    Create Result page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Flights from "./pages/flights/Flights";
+import Result from "./pages/result/Result";
 import "./styles/main.scss";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path="*" element={<Home />} />
       <Route path="/flights" element={<Flights />} />
+      <Route path="/result" element={<Result />} />
     </Routes>
   );
 };
---
+++ src/pages/result/Result.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/header/Header";
+import FlightSelect from "../../components/flightSelect/FlightSelect";
+
+const Result = () => {
+  return (
+    <div className="result">
+      <Header className={"header__dark"} />
+    </div>
+  );
+};
+
+export default Result;
