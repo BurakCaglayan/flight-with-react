c67df44acb3144884df5f2848b14a1370cf726f7
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2024-06-04 05:07:38 +0300

    Update FlightSelectCard component with data

--- src/pages/flights/components/FlightSelectCard.jsx
+++ src/pages/flights/components/FlightSelectCard.jsx
@@ -1,18 +1,26 @@
 import PropTypes from "prop-types";
 import Card from "../../../components/UI/Card";
 
-const FlightSelectCard = ({ className, title, price, item }) => {
+const FlightSelectCard = ({ className, item }) => {
+  const { brandCode, price, rights } = item;
   return (
     <div className={`flight-select-card ${className}`}>
       <div className="flight-select-card__header">
-        <div className="flight-select-card__header__tittle">{title}</div>
+        <div className="flight-select-card__header__tittle">{brandCode}</div>
         <div className="flight-select-card__price">
-          <div>TRY</div>
-          <div>{price}</div>
+          <div>{price.currency}</div>
+          <div>{price.amount}</div>
         </div>
       </div>
       <div className="flight-select-card__body">
-        <div className="flight-select-card__body__item">{item}</div>
+        {rights.map((right, index) => (
+          <div
+            key={`right_${index}`}
+            className="flight-select-card__body__item"
+          >
+            {right}
+          </div>
+        ))}
       </div>
       <div className="flight-select-card__footer">
         <button className="flight-select-card__button">Uçuşu Seç</button>
@@ -23,9 +31,7 @@ const FlightSelectCard = ({ className, title, price, item }) => {
 
 FlightSelectCard.propTypes = {
   className: PropTypes.string,
-  title: PropTypes.string,
-  price: PropTypes.string,
-  item: PropTypes.string,
+  item: PropTypes.shape({}),
 };
 
 export default FlightSelectCard;
